d5f1c68711586303647ee1c6acc901bdd926a0cd
anitayadav0508 <alishayadav0508@gmail.com>
2025-08-26 19:31:02 +0530

    add crud-functionality in jpa using findBy Methods

---
+++ crud-operation-findBy-methods-using-jpa/src/main/java/com/practise/crud_operation_findBy_methods_using_jpa/CrudOperationFindByMethodsUsingJpaApplication.java
@@ -0,0 +1,14 @@
+package com.practise.crud_operation_findBy_methods_using_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationFindByMethodsUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationFindByMethodsUsingJpaApplication.class, args);
+        
+	}
+
+}
---
+++ crud-operation-findBy-methods-using-jpa/src/test/java/com/practise/crud_operation_findBy_methods_using_jpa/CrudOperationFindByMethodsUsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.crud_operation_findBy_methods_using_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationFindByMethodsUsingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
