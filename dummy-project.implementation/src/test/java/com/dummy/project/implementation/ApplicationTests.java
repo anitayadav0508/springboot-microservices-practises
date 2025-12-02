165cafa1585b026e1f1498ccb3f60d49f4734f13
ayadav@3clogic.com <ayadav@N-25-AP-J6-078M.local>
2025-12-02 19:10:48 +0530

    dummy proejct push

---
+++ dummy-project.implementation/src/test/java/com/dummy/project/implementation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummy.project.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
