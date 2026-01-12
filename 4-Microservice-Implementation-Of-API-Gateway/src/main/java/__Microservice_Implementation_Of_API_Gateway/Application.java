d67844cf816e8dea33a545b93e37f97a93b63028
ayadav@3clogic.com <ayadav@N-25-AP-J6-078M.local>
2026-01-12 12:10:42 +0530

    Implement api gateway

---
+++ 4-Microservice-Implementation-Of-API-Gateway/src/main/java/__Microservice_Implementation_Of_API_Gateway/Application.java
@@ -0,0 +1,15 @@
+package __Microservice_Implementation_Of_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 4-Microservice-Implementation-Of-API-Gateway/src/test/java/__Microservice_Implementation_Of_API_Gateway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package __Microservice_Implementation_Of_API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
